a24a4ea3771549a9435d0a4d7f2d1a407dfd6feb
kawashita kenji <leonardkenji@gmail.com>
2026-02-25 00:38:41 +0900

    correcetd some seeds and made the team generator properly print in the teams view

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,4 +3,9 @@ class TeamsController < ApplicationController
   def show
     @team = Team.find(params[:id])
   end
+
+  def edit
+    @team = Team.find(params[:id])
+    @message = Message.new
+  end
 end
